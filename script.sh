826cc6e3f07ee2092d7a57cdf386f6703e0a210a
Cveto <praprotnik@gmail.com>
2024-04-09 11:40:07 +0200

    mtr used instead of netcat

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# Needs sed, awk and tshark
+# Needs sed, awk, mtr, netcat and tshark
 
 # $1 how many packets
 # $2 To what IP
 # $3 On What port
 
-# Delete file if it does not exist and make it writable
+# Delete file if exist and make it writable
 file="/tmp/tmp.txt"
 if [ -f "$file" ] ; then
     rm -f "$file"
@@ -19,17 +19,18 @@ pkill tshark && while pgrep -l tshark; do sleep 1;done;
 # Run package capture
 tshark -i any -Y 'tcp.port==1792 && (tcp.flags.syn==1 && tcp.flags.ack==1)' -T fields -e tcp.time_relative -e tcp.dstport &> /tmp/tmp.txt &
 
-
 sleep 1 # tShark needs some time to wake-up. And there doesn't seem to be a way to know how long that takes.
 
-#for i in $(seq 1 1 $1); do nc -zv 172.24.193.168 1792 2> /dev/null; done
-j=0;
+
+## Generate packets - with netcat or mtr
 
 echo "----- MTR results -----"
 echo "Results in miliseconds:"
 #mtr --tcp --report -c $1 --port $3 $2
 mtr --tcp -c $1 -r -f 32 -m 32 --port $3 $2
 
+#for i in $(seq 1 1 $1); do nc -zv 172.24.193.168 1792 2> /dev/null; done
+#j=0;
 #for i in $(seq 1 1 $1)
 #do
 #    nc -zv $2 $3 2> /dev/null;
@@ -39,11 +40,11 @@ mtr --tcp -c $1 -r -f 32 -m 32 --port $3 $2
 echo ""
 echo "----- tshark results -----"
 
-sleep 1 # Definetely needed, otherwise I don't capture all packets
+sleep 1 # Definetely needed, otherwise I tshark does not capture all packets
 
-pkill tshark && while pgrep -l tshark > /dev/null; do sleep 1;done;   # Delay needed
+pkill tshark && while pgrep -l tshark > /dev/null; do sleep 1;done;   # Delay needed or packets missing in the output file
 
-sed -i '/^[0-9]\./!d' $file  # Remove all lines that are not results
+sed -i '/^[0-9].*\./!d' $file  # Remove all lines that are not results (that don't staret with NUMBER and dot)  (so AWK can process them after).
 
 echo "SYN packets sent (according to our instructions and mtr): $1"
 echo "SYN_ACK packets received (according to tshark): `wc -l < /tmp/tmp.txt`"
